5d03b0002421485cf3e028f983045b03ad1e89ec
Jihoon Park <jihoonpark@snu.ac.kr>
2024-10-29 06:38:48 +0000

    Add install-related files for initial tvm installation

---
+++ config_cmake.sh
@@ -0,0 +1,22 @@
+cp ../cmake/config.cmake .
+
+# controls default compilation flags (Candidates: Release, Debug, RelWithDebInfo)
+echo "set(CMAKE_BUILD_TYPE RelWithDebInfo)" >> config.cmake
+
+# LLVM is a must dependency for compiler end
+echo "set(USE_LLVM \"llvm-config --ignore-libllvm --link-static\")" >> config.cmake
+echo "set(HIDE_PRIVATE_SYMBOLS ON)" >> config.cmake
+
+# GPU SDKs, turn on if needed
+echo "set(USE_CUDA   OFF)" >> config.cmake
+echo "set(USE_METAL  OFF)" >> config.cmake
+echo "set(USE_VULKAN OFF)" >> config.cmake
+echo "set(USE_OPENCL OFF)" >> config.cmake
+
+# cuBLAS, cuDNN, cutlass support, turn on if needed
+echo "set(USE_CUBLAS OFF)" >> config.cmake
+echo "set(USE_CUDNN  OFF)" >> config.cmake
+echo "set(USE_CUTLASS OFF)" >> config.cmake
+
+# set GTEST off
+echo "set(USE_GTEST OFF)" >> config.cmake
