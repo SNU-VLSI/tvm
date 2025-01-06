717f704c212d25f470bedca1c92ddfafefa3d5c1
jaeyongjang@mini <jong166@gmail.com>
2025-01-06 02:22:22 +0000

    Test pytorch model TVM flow

--- tvm_practice/test_imcflow/pytorch/test.py
+++ tvm_practice/test_imcflow/pytorch/test.py
@@ -36,112 +36,106 @@ def make_quantize(input, input_types):
   bias = tvm.relay.Constant(tvm.runtime.ndarray.array(np.array(input[2], dtype=np.int32)))
   return tvm.relay.qnn.op.quantize(input[0], scale, bias, 1, "float32")
 
-def printModel(mod, param_dict, mod_name):
+def printModel(result_dir, mod, param_dict, mod_name):
   RelayVisualizer(
     relay_mod = mod,
     relay_param = param_dict,
     plotter = DotPlotter(),
     parser = DotVizParser(),
-  ).render(f"results/{mod_name}")
+  ).render(f"{result_dir}/{mod_name}")
 
-  with open(f"results/{mod_name}.txt", "w") as f:
+  with open(f"{result_dir}/{mod_name}.txt", "w") as f:
     f.write(pretty_print(mod))
 
-def test_1():
+def run_test(test_name, mod, param_dict):
+  eval_mod, eval_param_dict = mod, param_dict
+
+  # origin
+  printModel(test_name, eval_mod, eval_param_dict, "origin")
+
+  # bind param
+  eval_mod["main"] = bind_params_by_name(eval_mod["main"], eval_param_dict)
+  eval_mod = transform.InferType()(eval_mod)
+  printModel(test_name, eval_mod, eval_param_dict, "after_bind")
+
+  eval_mod = transform.MergeComposite(imcflow.pattern_table())(eval_mod)
+  printModel(test_name, eval_mod, eval_param_dict, "after_merge")
+
+  SplitConcatRegions = imcflow_transform.getSplitConcatDepsRegions(eval_mod["main"])
+  eval_mod = imcflow.ImcflowAnnotationPass(SplitConcatRegions)(eval_mod)
+  eval_mod = transform.MergeCompilerRegions()(eval_mod)
+  eval_mod = transform.PartitionGraph()(eval_mod)
+  printModel(test_name, eval_mod, eval_param_dict, "after_split_concat_partition")
+
+  AnnotGenerator = imcflow_transform.AnnotGenerator()
+  AnnotGenerator(eval_mod)
+  # print(AnnotGenerator.RegionList)
+  eval_mod = imcflow.ImcflowAnnotationPass(AnnotGenerator.RegionList)(eval_mod)
+  printModel(test_name, eval_mod, eval_param_dict, "after_annot")
+
+  eval_mod = transform.MergeCompilerRegions()(eval_mod)
+  printModel(test_name, eval_mod, eval_param_dict, "after_merge_region")
+
+  eval_mod = imcflow.ImcflowCleanRegionTag()(eval_mod)
+  printModel(test_name, eval_mod, eval_param_dict, "after_clean_region")
+
+  eval_mod = transform.PartitionGraph()(eval_mod)
+  printModel(test_name, eval_mod, eval_param_dict, "after_partition_graph")
+
+  eval_mod = imcflow.flattenSubgraphs(eval_mod)
+  printModel(test_name, eval_mod, eval_param_dict, "after_flatten")
+
+  eval_mod = imcflow.prune_imcflow_subgraphs(eval_mod)
+  printModel(test_name, eval_mod, eval_param_dict, "after_prune_model")
+
+  eval_mod = imcflow_transform.PackingInserter()(eval_mod)
+  printModel(test_name, eval_mod, eval_param_dict, "after_packing")
+
+  imcflow_transform.constructUsefulMappings(eval_mod)
+  imcflow_transform.constructCustomIDInFunc(eval_mod)
+  printModel(test_name, eval_mod, eval_param_dict, "with_custom_id")
+
+  imcflow_transform.NodeMapper()(eval_mod)
+  imcflow_transform.constructTensorEdgeList(eval_mod)
+  imcflow_transform.constructActiveIMCEDict(eval_mod)
+
+  print("Active IMCE list")
+  print(ImcflowDeviceConfig().ActiveIMCEPerFunc)
+
+  print("HW MAP")
+  print(ImcflowDeviceConfig().HWNodeMap)
+
+  print("CustomID TO Name")
+  print(imcflow.CustomIDToName())
+
+  print("Tensor Edge List")
+  for key, paths in ImcflowDeviceConfig().TensorEdgeListDict.items():
+    print(key)
+    for path in paths:
+      print(path)
+  
+  imcflow_transform.constructTensorIDToTensorEdgeDict()
+  print("Tensor ID to Tensor Edge")
+  for key, paths in ImcflowDeviceConfig().TensorIDtoEdge.items():
+    print(f"{key} : {paths}")
+  
+  imcflow_transform.constructNoCPathDict(eval_mod)
+  print("NoC Paths")
+  for key, paths in ImcflowDeviceConfig().NoCPaths.items():
+    print(key)
+    for k, v in paths.items():
+      print(k, v)
+
+  MemoryAllocator = imcflow_transform.MemoryAllocator()(eval_mod)
+  PolicyTableGenerator = imcflow_transform.PolicyTableGenerator(ImcflowDeviceConfig().NoCPaths)(eval_mod)
+
+def test_big():
   mod, param_dict = getModel()
-  eval_mod, eval_param_dict = mod, param_dict
+  run_test("big", mod, param_dict)
 
-  # origin
-  printModel(eval_mod, eval_param_dict, "origin")
-
-  # bind param
-  eval_mod["main"] = bind_params_by_name(eval_mod["main"], eval_param_dict)
-  eval_mod = transform.InferType()(eval_mod)
-  printModel(eval_mod, eval_param_dict, "after_bind")
-
-  eval_mod = transform.MergeComposite(imcflow.pattern_table())(eval_mod)
-  printModel(eval_mod, eval_param_dict, "after_merge")
-
-  SplitConcatRegions = imcflow_transform.getSplitConcatDepsRegions(eval_mod["main"])
-  eval_mod = imcflow.ImcflowAnnotationPass(SplitConcatRegions)(eval_mod)
-  eval_mod = transform.MergeCompilerRegions()(eval_mod)
-  eval_mod = transform.PartitionGraph()(eval_mod)
-  printModel(eval_mod, eval_param_dict, "after_split_concat_partition")
-
-  AnnotGenerator = imcflow_transform.AnnotGenerator()
-  AnnotGenerator(eval_mod)
-  # print(AnnotGenerator.RegionList)
-  eval_mod = imcflow.ImcflowAnnotationPass(AnnotGenerator.RegionList)(eval_mod)
-  printModel(eval_mod, eval_param_dict, "after_annot")
-
-  eval_mod = transform.MergeCompilerRegions()(eval_mod)
-  printModel(eval_mod, eval_param_dict, "after_merge_region")
-
-  eval_mod = imcflow.ImcflowCleanRegionTag()(eval_mod)
-  printModel(eval_mod, eval_param_dict, "after_clean_region")
-
-  eval_mod = transform.PartitionGraph()(eval_mod)
-  printModel(eval_mod, eval_param_dict, "after_partition_graph")
-
-  eval_mod = imcflow.flattenSubgraphs(eval_mod)
-  printModel(eval_mod, eval_param_dict, "after_flatten")
-
-  eval_mod = imcflow.prune_imcflow_subgraphs(eval_mod)
-  printModel(eval_mod, eval_param_dict, "after_prune_model")
-
-  eval_mod = imcflow_transform.PackingInserter()(eval_mod)
-  printModel(eval_mod, eval_param_dict, "after_packing")
-
-  imcflow_transform.constructUsefulMappings(eval_mod)
-  imcflow_transform.constructCustomIDInFunc(eval_mod)
-
-def test_2():
+def test_small():
   mod, param_dict = real_model2.getModel()
-  eval_mod, eval_param_dict = mod, param_dict
-
-  # origin
-  printModel(eval_mod, eval_param_dict, "origin")
-
-  # bind param
-  eval_mod["main"] = bind_params_by_name(eval_mod["main"], eval_param_dict)
-  eval_mod = transform.InferType()(eval_mod)
-  printModel(eval_mod, eval_param_dict, "after_bind")
-
-  eval_mod = transform.MergeComposite(imcflow.pattern_table())(eval_mod)
-  printModel(eval_mod, eval_param_dict, "after_merge")
-
-  SplitConcatRegions = imcflow_transform.getSplitConcatDepsRegions(eval_mod["main"])
-  eval_mod = imcflow.ImcflowAnnotationPass(SplitConcatRegions)(eval_mod)
-  eval_mod = transform.MergeCompilerRegions()(eval_mod)
-  eval_mod = transform.PartitionGraph()(eval_mod)
-  printModel(eval_mod, eval_param_dict, "after_split_concat_partition")
-
-  AnnotGenerator = imcflow_transform.AnnotGenerator()
-  AnnotGenerator(eval_mod)
-  # print(AnnotGenerator.RegionList)
-  eval_mod = imcflow.ImcflowAnnotationPass(AnnotGenerator.RegionList)(eval_mod)
-  printModel(eval_mod, eval_param_dict, "after_annot")
-
-  eval_mod = transform.MergeCompilerRegions()(eval_mod)
-  printModel(eval_mod, eval_param_dict, "after_merge_region")
-
-  eval_mod = imcflow.ImcflowCleanRegionTag()(eval_mod)
-  printModel(eval_mod, eval_param_dict, "after_clean_region")
-
-  eval_mod = transform.PartitionGraph()(eval_mod)
-  printModel(eval_mod, eval_param_dict, "after_partition_graph")
-
-  eval_mod = imcflow.flattenSubgraphs(eval_mod)
-  printModel(eval_mod, eval_param_dict, "after_flatten")
-
-  eval_mod = imcflow.prune_imcflow_subgraphs(eval_mod)
-  printModel(eval_mod, eval_param_dict, "after_prune_model")
-
-  eval_mod = imcflow_transform.PackingInserter()(eval_mod)
-  printModel(eval_mod, eval_param_dict, "after_packing")
-
-  imcflow_transform.constructUsefulMappings(eval_mod)
-  imcflow_transform.constructCustomIDInFunc(eval_mod)
+  run_test("small", mod, param_dict)
 
 if __name__ == "__main__":
   tvm.testing.main()
