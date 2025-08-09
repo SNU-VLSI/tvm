6ef117baf07591a602241eff64f17216a61f1630
Jihoon Park <jihoonpark@snu.ac.kr>
2025-08-09 10:04:30 +0000

    add relative path for Cmake variables in a53 config

--- tvm_practice/test_graph/arm_cmake/cmake/config_a53.cmake
+++ tvm_practice/test_graph/arm_cmake/cmake/config_a53.cmake
@@ -3,7 +3,7 @@
 #   cmake -S ${SOURCE_DIR} -B ${BINARY_DIR} \
 #         -C ${SOURCE_DIR}/cmake/config_a53.cmake
 
-set(MLF_TAR "/root/project/tvm/tvm_practice/test_graph/lib_graph_system-lib.tar" CACHE FILEPATH "Path to MLF tarball")
+set(MLF_TAR "${CMAKE_CURRENT_LIST_DIR}/../../lib_graph_system-lib.tar" CACHE FILEPATH "Path to MLF tarball")
 set(TVM_BUILD_HOST_RUNNER OFF CACHE BOOL "Build host runner")
 set(TVM_BUILD_ARM_RUNNER ON CACHE BOOL "Build ARM runner")
-set(CMAKE_TOOLCHAIN_FILE "/root/project/tvm/tvm_practice/test_graph/arm_cmake/cmake/arm-cortex-a53.cmake" CACHE FILEPATH "Path to toolchain file")
+set(CMAKE_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/arm-cortex-a53.cmake" CACHE FILEPATH "Path to toolchain file")
