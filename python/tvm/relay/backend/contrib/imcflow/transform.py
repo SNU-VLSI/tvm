4ffc47e8a97404733ee0bb63de988421771d5998
Jaeyong Jang <jong1664@gmail.com>
2025-12-30 10:34:38 +0000

    set mem block's block tiling info

--- python/tvm/relay/backend/contrib/imcflow/transform.py
+++ python/tvm/relay/backend/contrib/imcflow/transform.py
@@ -3267,6 +3267,7 @@ class MemoryAllocator:
                 c_input_var_offsets=c_input_var_offsets,
                 c_input_var_sizes=c_input_var_sizes
               )
+              mem_block.block_tiling_info = block_tiling_info
 
               # add edge info and allocate
               ImcflowDeviceConfig().add_tensor_edge_info(edge, TensorEdgeInfo(data_block=mem_block, block_tiling_info=block_tiling_info))
@@ -3326,6 +3327,7 @@ class MemoryAllocator:
                 c_input_var_offsets=c_output_var_offsets,
                 c_input_var_sizes=c_output_var_sizes
               )
+              mem_block.block_tiling_info = block_tiling_info
 
               # add edge info and allocate
               ImcflowDeviceConfig().add_tensor_edge_info(edge, TensorEdgeInfo(data_block=mem_block, block_tiling_info=block_tiling_info))
