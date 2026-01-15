f668ca473cd4b067ba4fba4edb1abb495df44141
Jaeyong Jang <jong1664@gmail.com>
2026-01-15 23:54:07 +0900

    update BE for new round partition

--- python/tvm/relay/backend/contrib/imcflow/codegen.py
+++ python/tvm/relay/backend/contrib/imcflow/codegen.py
@@ -666,6 +666,11 @@ class InodeCodeBlockBuilder(tvm.relay.ExprVisitor):
       block = WriteIMCUBlock(node, "imcu write")
       self.codeblocks.append(node, block, CodePhase.INIT)
 
+    # sync before imce compute
+    for inode in NodeID.inodes():
+      block = SyncAllINodes(inode, "sync before compute enable")
+      self.codeblocks.append(inode, block, CodePhase.INIT)
+
     # imce compute
     active_imces = DevConfig().ActiveIMCEPerFunc[func_name]
     for imce, inst_edge in sorted(DevConfig().InstEdgeInfoDict[func_name].items(), key=lambda x: x[0].name):
