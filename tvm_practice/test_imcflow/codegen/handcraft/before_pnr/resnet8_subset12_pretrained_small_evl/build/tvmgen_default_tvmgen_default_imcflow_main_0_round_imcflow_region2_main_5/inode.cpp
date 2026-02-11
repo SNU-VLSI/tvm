bc8c4a4702bc6e3a6f2f6aa68804432f6a80590c
Jihoon Park <jihoonpark@snu.ac.kr>
2026-02-11 14:14:00 +0000

    force add before_pnr handcraft

---
+++ tvm_practice/test_imcflow/codegen/handcraft/before_pnr/resnet8_subset12_pretrained_small_evl/build/tvmgen_default_tvmgen_default_imcflow_main_0_round_imcflow_region2_main_5/inode.cpp
@@ -0,0 +1,521 @@
+#include "../common_decl.h"
+void tvmgen_default_tvmgen_default_imcflow_main_0_round_imcflow_region2_main_5() {
+  int hid = __builtin_INODE_GET_CORE_HID();
+  int wid = 0;
+  int var1; // policy_table_start_address
+  int var2; // imem_start_address
+  int var3; // imcu_start_address
+  int var4; // send_data_base_address
+  int* var5; // sm28_d32lhs_send_data_base_address
+  int var6; // sm28_d32lhs_tile_loop_count
+  int* var7; // sm29_d32rhs_send_data_base_address
+  int var8; // sm29_d32rhs_tile_loop_count
+  int* var9; // s34_d35func_out0_recv_data_base_address
+  int var10; // s34_d35func_out0_tile_loop_count
+  int var11; // recv_data_base_address
+  if (hid == 0 && wid == 0) { // inode_0_0
+    // generate: clear flag before policy update
+    __builtin_INODE_SET_FLAG(0);
+    // endgenerate: clear flag before policy update
+    // generate: policy update
+    var1 = 0;
+    for (int i1 = 0; i1 < 6; i1++) { // generate
+      __builtin_INODE_PU(var1 + i1*32, 0, i1, 0);
+    } // endgenerate
+    var1 = 192;
+    __builtin_INODE_PU(var1, 0, 0, 1);
+    __builtin_INODE_PU(var1, 32, 1, 1);
+    __builtin_INODE_PU(var1, 64, 2, 1);
+    __builtin_INODE_PU(var1, 96, 3, 1);
+    __builtin_INODE_PU(var1, 128, 4, 1);
+    var1 = 352;
+    __builtin_INODE_PU(var1, 0, 0, 2);
+    __builtin_INODE_PU(var1, 32, 1, 2);
+    __builtin_INODE_PU(var1, 64, 2, 2);
+    __builtin_INODE_PU(var1, 96, 3, 2);
+    var1 = 480;
+    __builtin_INODE_PU(var1, 0, 0, 3);
+    __builtin_INODE_PU(var1, 32, 1, 3);
+    __builtin_INODE_PU(var1, 64, 2, 3);
+    var1 = 576;
+    __builtin_INODE_PU(var1, 0, 0, 4);
+    __builtin_INODE_PU(var1, 32, 1, 4);
+    // endgenerate: policy update
+    // generate: sync all inodes
+    __builtin_INODE_SET_FLAG(255);
+    __builtin_INODE_STANDBY(5, 255);
+    __builtin_INODE_STANDBY(10, 255);
+    __builtin_INODE_STANDBY(15, 255);
+    __asm__ volatile("nop\n" "nop\n" "nop\n" "nop\n");
+    __builtin_INODE_SET_FLAG(0);
+    // endgenerate: sync all inodes
+    // generate: halt for slave inodes
+    __builtin_INODE_HALT();
+    // endgenerate: halt for slave inodes
+    // generate: imem write: imce_0_1
+    var2 = 640;
+    __builtin_INODE_SET_ADDR_CNT(0);
+    // generate. loop count == 1
+    __builtin_INODE_WR_IMEM(var2 + 0*32, 0, 2);
+    // endgenerate
+    // endgenerate: imem write: imce_0_1
+    // generate: imem write: imce_0_2
+    var2 = 672;
+    __builtin_INODE_SET_ADDR_CNT(0);
+    // generate. loop count == 1
+    __builtin_INODE_WR_IMEM(var2 + 0*32, 0, 3);
+    // endgenerate
+    // endgenerate: imem write: imce_0_2
+    // generate: imem write: imce_0_3
+    var2 = 704;
+    __builtin_INODE_SET_ADDR_CNT(0);
+    // generate. loop count == 1
+    __builtin_INODE_WR_IMEM(var2 + 0*32, 0, 4);
+    // endgenerate
+    // endgenerate: imem write: imce_0_3
+    // generate: imem write: imce_0_4
+    var2 = 736;
+    __builtin_INODE_SET_ADDR_CNT(0);
+    // generate. loop count == 1
+    __builtin_INODE_WR_IMEM(var2 + 0*32, 0, 5);
+    // endgenerate
+    // endgenerate: imem write: imce_0_4
+
+    // generate: sync before compute enable
+    __builtin_INODE_SET_FLAG(255);
+    __builtin_INODE_STANDBY(5, 255);
+    __builtin_INODE_STANDBY(10, 255);
+    __builtin_INODE_STANDBY(15, 255);
+    __asm__ volatile("nop\n" "nop\n" "nop\n" "nop\n");
+    __builtin_INODE_SET_FLAG(0);
+    // endgenerate: sync before compute enable
+    // generate: wait all imce compute enable
+    __builtin_INODE_SET_FLAG(255);
+    __builtin_INODE_STANDBY(5, 255);
+    __builtin_INODE_STANDBY(10, 255);
+    __builtin_INODE_STANDBY(15, 255);
+    __asm__ volatile("nop\n" "nop\n" "nop\n" "nop\n");
+    __builtin_INODE_SET_FLAG(0);
+    // endgenerate: wait all imce compute enable
+    // generate: sync all inodes
+    __builtin_INODE_SET_FLAG(255);
+    __builtin_INODE_STANDBY(5, 255);
+    __builtin_INODE_STANDBY(10, 255);
+    __builtin_INODE_STANDBY(15, 255);
+    __asm__ volatile("nop\n" "nop\n" "nop\n" "nop\n");
+    __builtin_INODE_SET_FLAG(0);
+    // endgenerate: sync all inodes
+    // generate: halt for slave inodes
+    __builtin_INODE_HALT();
+    // endgenerate: halt for slave inodes
+    // generate: send - TensorEdge((-28, odata), (32, lhs)), inode_0_0 -> imce_3_4
+    var5 = (int*)(2048);
+    var4 = 0;
+    var6 = var5[0];
+    __asm__ volatile("nop");
+    for (int i1 = 0; i1 < var6; i1++) { // generate
+      __builtin_INODE_SEND(var4 + i1*32, 0, 1, 2);
+
+    } // endgenerate
+    // endgenerate: send - TensorEdge((-28, odata), (32, lhs)), inode_0_0 -> imce_3_4
+    // generate: sync all inodes
+    __builtin_INODE_SET_FLAG(255);
+    __builtin_INODE_STANDBY(5, 255);
+    __builtin_INODE_STANDBY(10, 255);
+    __builtin_INODE_STANDBY(15, 255);
+    __asm__ volatile("nop\n" "nop\n" "nop\n" "nop\n");
+    __builtin_INODE_SET_FLAG(0);
+    // endgenerate: sync all inodes
+    // generate: halt for slave inodes
+    __builtin_INODE_HALT();
+    // endgenerate: halt for slave inodes
+  }
+  else if (hid == 1 && wid == 0) { // inode_1_0
+    // generate: clear flag before policy update
+    __builtin_INODE_SET_FLAG(0);
+    // endgenerate: clear flag before policy update
+    // generate: policy update
+    var1 = 0;
+    for (int i1 = 0; i1 < 7; i1++) { // generate
+      __builtin_INODE_PU(var1 + i1*32, 0, i1, 0);
+    } // endgenerate
+    var1 = 224;
+    for (int i1 = 0; i1 < 6; i1++) { // generate
+      __builtin_INODE_PU(var1 + i1*32, 0, i1, 1);
+    } // endgenerate
+    var1 = 416;
+    __builtin_INODE_PU(var1, 0, 0, 2);
+    __builtin_INODE_PU(var1, 32, 1, 2);
+    __builtin_INODE_PU(var1, 64, 2, 2);
+    __builtin_INODE_PU(var1, 96, 3, 2);
+    __builtin_INODE_PU(var1, 128, 4, 2);
+    var1 = 576;
+    __builtin_INODE_PU(var1, 0, 0, 3);
+    __builtin_INODE_PU(var1, 32, 1, 3);
+    __builtin_INODE_PU(var1, 64, 2, 3);
+    __builtin_INODE_PU(var1, 96, 3, 3);
+    var1 = 704;
+    __builtin_INODE_PU(var1, 0, 0, 4);
+    __builtin_INODE_PU(var1, 32, 1, 4);
+    __builtin_INODE_PU(var1, 64, 2, 4);
+    // endgenerate: policy update
+    // generate: sync all inodes
+    __builtin_INODE_SET_FLAG(255);
+    __builtin_INODE_STANDBY(0, 255);
+    __builtin_INODE_STANDBY(10, 255);
+    __builtin_INODE_STANDBY(15, 255);
+    __asm__ volatile("nop\n" "nop\n" "nop\n" "nop\n");
+    __builtin_INODE_SET_FLAG(0);
+    // endgenerate: sync all inodes
+    // generate: halt for slave inodes
+    __builtin_INODE_HALT();
+    // endgenerate: halt for slave inodes
+    // generate: imem write: imce_1_1
+    var2 = 800;
+    __builtin_INODE_SET_ADDR_CNT(0);
+    // generate. loop count == 1
+    __builtin_INODE_WR_IMEM(var2 + 0*32, 0, 3);
+    // endgenerate
+    // endgenerate: imem write: imce_1_1
+    // generate: imem write: imce_1_2
+    var2 = 832;
+    __builtin_INODE_SET_ADDR_CNT(0);
+    // generate. loop count == 1
+    __builtin_INODE_WR_IMEM(var2 + 0*32, 0, 4);
+    // endgenerate
+    // endgenerate: imem write: imce_1_2
+    // generate: imem write: imce_1_3
+    var2 = 864;
+    __builtin_INODE_SET_ADDR_CNT(0);
+    // generate. loop count == 1
+    __builtin_INODE_WR_IMEM(var2 + 0*32, 0, 5);
+    // endgenerate
+    // endgenerate: imem write: imce_1_3
+    // generate: imem write: imce_1_4
+    var2 = 896;
+    __builtin_INODE_SET_ADDR_CNT(0);
+    // generate. loop count == 1
+    __builtin_INODE_WR_IMEM(var2 + 0*32, 0, 6);
+    // endgenerate
+    // endgenerate: imem write: imce_1_4
+
+    // generate: sync before compute enable
+    __builtin_INODE_SET_FLAG(255);
+    __builtin_INODE_STANDBY(0, 255);
+    __builtin_INODE_STANDBY(10, 255);
+    __builtin_INODE_STANDBY(15, 255);
+    __asm__ volatile("nop\n" "nop\n" "nop\n" "nop\n");
+    __builtin_INODE_SET_FLAG(0);
+    // endgenerate: sync before compute enable
+    // generate: wait all imce compute enable
+    __builtin_INODE_SET_FLAG(255);
+    __builtin_INODE_STANDBY(0, 255);
+    __builtin_INODE_STANDBY(10, 255);
+    __builtin_INODE_STANDBY(15, 255);
+    __asm__ volatile("nop\n" "nop\n" "nop\n" "nop\n");
+    __builtin_INODE_SET_FLAG(0);
+    // endgenerate: wait all imce compute enable
+    // generate: sync all inodes
+    __builtin_INODE_SET_FLAG(255);
+    __builtin_INODE_STANDBY(0, 255);
+    __builtin_INODE_STANDBY(10, 255);
+    __builtin_INODE_STANDBY(15, 255);
+    __asm__ volatile("nop\n" "nop\n" "nop\n" "nop\n");
+    __builtin_INODE_SET_FLAG(0);
+    // endgenerate: sync all inodes
+    // generate: halt for slave inodes
+    __builtin_INODE_HALT();
+    // endgenerate: halt for slave inodes
+    // generate: send - TensorEdge((-29, odata), (32, rhs)), inode_1_0 -> imce_3_4
+    var7 = (int*)(2048);
+    var4 = 0;
+    var8 = var7[0];
+    __asm__ volatile("nop");
+    for (int i1 = 0; i1 < var8; i1++) { // generate
+      __builtin_INODE_SEND(var4 + i1*32, 0, 2, 3);
+
+    } // endgenerate
+    // endgenerate: send - TensorEdge((-29, odata), (32, rhs)), inode_1_0 -> imce_3_4
+    // generate: sync all inodes
+    __builtin_INODE_SET_FLAG(255);
+    __builtin_INODE_STANDBY(0, 255);
+    __builtin_INODE_STANDBY(10, 255);
+    __builtin_INODE_STANDBY(15, 255);
+    __asm__ volatile("nop\n" "nop\n" "nop\n" "nop\n");
+    __builtin_INODE_SET_FLAG(0);
+    // endgenerate: sync all inodes
+    // generate: halt for slave inodes
+    __builtin_INODE_HALT();
+    // endgenerate: halt for slave inodes
+  }
+  else if (hid == 2 && wid == 0) { // inode_2_0
+    // generate: clear flag before policy update
+    __builtin_INODE_SET_FLAG(0);
+    // endgenerate: clear flag before policy update
+    // generate: policy update
+    var1 = 0;
+    for (int i1 = 0; i1 < 8; i1++) { // generate
+      __builtin_INODE_PU(var1 + i1*32, 0, i1, 0);
+    } // endgenerate
+    var1 = 256;
+    for (int i1 = 0; i1 < 7; i1++) { // generate
+      __builtin_INODE_PU(var1 + i1*32, 0, i1, 1);
+    } // endgenerate
+    var1 = 480;
+    for (int i1 = 0; i1 < 6; i1++) { // generate
+      __builtin_INODE_PU(var1 + i1*32, 0, i1, 2);
+    } // endgenerate
+    var1 = 672;
+    __builtin_INODE_PU(var1, 0, 0, 3);
+    __builtin_INODE_PU(var1, 32, 1, 3);
+    __builtin_INODE_PU(var1, 64, 2, 3);
+    var1 = 768;
+    __builtin_INODE_PU(var1, 0, 0, 4);
+    __builtin_INODE_PU(var1, 32, 1, 4);
+    __builtin_INODE_PU(var1, 64, 2, 4);
+    // endgenerate: policy update
+    // generate: sync all inodes
+    __builtin_INODE_SET_FLAG(255);
+    __builtin_INODE_STANDBY(0, 255);
+    __builtin_INODE_STANDBY(5, 255);
+    __builtin_INODE_STANDBY(15, 255);
+    __asm__ volatile("nop\n" "nop\n" "nop\n" "nop\n");
+    __builtin_INODE_SET_FLAG(0);
+    // endgenerate: sync all inodes
+    // generate: halt for slave inodes
+    __builtin_INODE_HALT();
+    // endgenerate: halt for slave inodes
+    // generate: imem write: imce_2_1
+    var2 = 864;
+    __builtin_INODE_SET_ADDR_CNT(0);
+    // generate. loop count == 1
+    __builtin_INODE_WR_IMEM(var2 + 0*32, 0, 2);
+    // endgenerate
+    // endgenerate: imem write: imce_2_1
+    // generate: imem write: imce_2_2
+    var2 = 896;
+    __builtin_INODE_SET_ADDR_CNT(0);
+    // generate. loop count == 1
+    __builtin_INODE_WR_IMEM(var2 + 0*32, 0, 3);
+    // endgenerate
+    // endgenerate: imem write: imce_2_2
+    // generate: imem write: imce_2_3
+    var2 = 928;
+    __builtin_INODE_SET_ADDR_CNT(0);
+    // generate. loop count == 1
+    __builtin_INODE_WR_IMEM(var2 + 0*32, 0, 4);
+    // endgenerate
+    // endgenerate: imem write: imce_2_3
+    // generate: imem write: imce_2_4
+    var2 = 960;
+    __builtin_INODE_SET_ADDR_CNT(0);
+    // generate. loop count == 1
+    __builtin_INODE_WR_IMEM(var2 + 0*32, 0, 5);
+    // endgenerate
+    // endgenerate: imem write: imce_2_4
+
+    // generate: sync before compute enable
+    __builtin_INODE_SET_FLAG(255);
+    __builtin_INODE_STANDBY(0, 255);
+    __builtin_INODE_STANDBY(5, 255);
+    __builtin_INODE_STANDBY(15, 255);
+    __asm__ volatile("nop\n" "nop\n" "nop\n" "nop\n");
+    __builtin_INODE_SET_FLAG(0);
+    // endgenerate: sync before compute enable
+    // generate: wait all imce compute enable
+    __builtin_INODE_SET_FLAG(255);
+    __builtin_INODE_STANDBY(0, 255);
+    __builtin_INODE_STANDBY(5, 255);
+    __builtin_INODE_STANDBY(15, 255);
+    __asm__ volatile("nop\n" "nop\n" "nop\n" "nop\n");
+    __builtin_INODE_SET_FLAG(0);
+    // endgenerate: wait all imce compute enable
+    // generate: sync all inodes
+    __builtin_INODE_SET_FLAG(255);
+    __builtin_INODE_STANDBY(0, 255);
+    __builtin_INODE_STANDBY(5, 255);
+    __builtin_INODE_STANDBY(15, 255);
+    __asm__ volatile("nop\n" "nop\n" "nop\n" "nop\n");
+    __builtin_INODE_SET_FLAG(0);
+    // endgenerate: sync all inodes
+    // generate: halt for slave inodes
+    __builtin_INODE_HALT();
+    // endgenerate: halt for slave inodes
+    // generate: sync all inodes
+    __builtin_INODE_SET_FLAG(255);
+    __builtin_INODE_STANDBY(0, 255);
+    __builtin_INODE_STANDBY(5, 255);
+    __builtin_INODE_STANDBY(15, 255);
+    __asm__ volatile("nop\n" "nop\n" "nop\n" "nop\n");
+    __builtin_INODE_SET_FLAG(0);
+    // endgenerate: sync all inodes
+    // generate: halt for slave inodes
+    __builtin_INODE_HALT();
+    // endgenerate: halt for slave inodes
+  }
+  else if (hid == 3 && wid == 0) { // inode_3_0
+    // generate: clear flag before policy update
+    __builtin_INODE_SET_FLAG(0);
+    // endgenerate: clear flag before policy update
+    // generate: policy update
+    var1 = 8288;
+    for (int i1 = 0; i1 < 11; i1++) { // generate
+      __builtin_INODE_PU(var1 + i1*32, 0, i1, 0);
+    } // endgenerate
+    var1 = 8640;
+    for (int i1 = 0; i1 < 9; i1++) { // generate
+      __builtin_INODE_PU(var1 + i1*32, 0, i1, 1);
+    } // endgenerate
+    var1 = 8928;
+    for (int i1 = 0; i1 < 11; i1++) { // generate
+      __builtin_INODE_PU(var1 + i1*32, 0, i1, 2);
+    } // endgenerate
+    var1 = 9280;
+    for (int i1 = 0; i1 < 8; i1++) { // generate
+      __builtin_INODE_PU(var1 + i1*32, 0, i1, 3);
+    } // endgenerate
+    var1 = 9536;
+    __builtin_INODE_PU(var1, 0, 0, 4);
+    __builtin_INODE_PU(var1, 32, 1, 4);
+    __builtin_INODE_PU(var1, 64, 2, 4);
+    __builtin_INODE_PU(var1, 96, 3, 4);
+    __builtin_INODE_PU(var1, 128, 4, 4);
+    // endgenerate: policy update
+    // generate: sync all inodes
+    __builtin_INODE_SET_FLAG(255);
+    __builtin_INODE_STANDBY(0, 255);
+    __builtin_INODE_STANDBY(5, 255);
+    __builtin_INODE_STANDBY(10, 255);
+    __asm__ volatile("nop\n" "nop\n" "nop\n" "nop\n");
+    __builtin_INODE_SET_FLAG(0);
+    // endgenerate: sync all inodes
+    // generate: done and intrt for master inode
+    __builtin_INODE_DONE();
+    __builtin_INODE_INTRT(0);
+    // endgenerate: done and intrt for master inode
+    // generate: halt for master inode after done and intrt
+    __builtin_INODE_HALT();
+    // endgenerate: halt for master inode after done and intrt
+    // generate: imem write: imce_3_1
+    var2 = 9696;
+    __builtin_INODE_SET_ADDR_CNT(0);
+    // generate. loop count == 1
+    __builtin_INODE_WR_IMEM(var2 + 0*32, 0, 3);
+    // endgenerate
+    // endgenerate: imem write: imce_3_1
+    // generate: imem write: imce_3_2
+    var2 = 9728;
+    __builtin_INODE_SET_ADDR_CNT(0);
+    for (int i1 = 0; i1 < 32; i1++) { // generate
+      __builtin_INODE_WR_IMEM(var2 + i1*32, 0, 4);
+    } // endgenerate
+    // endgenerate: imem write: imce_3_2
+    // generate: imem write: imce_3_3
+    var2 = 10752;
+    __builtin_INODE_SET_ADDR_CNT(0);
+    for (int i1 = 0; i1 < 14; i1++) { // generate
+      __builtin_INODE_WR_IMEM(var2 + i1*32, 0, 5);
+    } // endgenerate
+    // endgenerate: imem write: imce_3_3
+    // generate: imem write: imce_3_4
+    var2 = 11200;
+    __builtin_INODE_SET_ADDR_CNT(0);
+    for (int i1 = 0; i1 < 9; i1++) { // generate
+      __builtin_INODE_WR_IMEM(var2 + i1*32, 0, 6);
+    } // endgenerate
+    // endgenerate: imem write: imce_3_4
+    // generate: imcu write
+    var3 = 0;
+    __builtin_INODE_SET_ADDR_CNT(0);
+    for (int i1 = 0; i1 < 256; i1++) { // generate
+      __builtin_INODE_WR_IMCU(var3 + i1*32, 0, 7);
+    } // endgenerate
+    // endgenerate: imcu write
+    // generate: sync before compute enable
+    __builtin_INODE_SET_FLAG(255);
+    __builtin_INODE_STANDBY(0, 255);
+    __builtin_INODE_STANDBY(5, 255);
+    __builtin_INODE_STANDBY(10, 255);
+    __asm__ volatile("nop\n" "nop\n" "nop\n" "nop\n");
+    __builtin_INODE_SET_FLAG(0);
+    // endgenerate: sync before compute enable
+    // generate: imce_3_2 compute
+    __builtin_INODE_IMCE_COMPUTE(0, 4);
+    // endgenerate: imce_3_2 compute
+    // generate: imce_3_3 compute
+    __builtin_INODE_IMCE_COMPUTE(0, 5);
+    // endgenerate: imce_3_3 compute
+    // generate: imce_3_4 compute
+    __builtin_INODE_IMCE_COMPUTE(0, 6);
+    // endgenerate: imce_3_4 compute
+    // generate: wait all imce compute enable
+    __builtin_INODE_SET_FLAG(255);
+    __builtin_INODE_STANDBY(0, 255);
+    __builtin_INODE_STANDBY(5, 255);
+    __builtin_INODE_STANDBY(10, 255);
+    __asm__ volatile("nop\n" "nop\n" "nop\n" "nop\n");
+    __builtin_INODE_SET_FLAG(0);
+    // endgenerate: wait all imce compute enable
+    // generate: send - TensorEdge((-33, config), (34, config)), inode_3_0 -> imce_3_2
+    var4 = 8256;
+    // generate. loop count == 1
+    __builtin_INODE_SEND(var4 + 0*32, 0, 9, 1);
+
+    // endgenerate
+    // endgenerate: send - TensorEdge((-33, config), (34, config)), inode_3_0 -> imce_3_2
+    // generate: send - TensorEdge((-30, min), (33, min)), inode_3_0 -> imce_3_3
+    var4 = 8192;
+    // generate. loop count == 1
+    __builtin_INODE_SEND(var4 + 0*32, 0, 8, 1);
+
+    // endgenerate
+    // endgenerate: send - TensorEdge((-30, min), (33, min)), inode_3_0 -> imce_3_3
+    // generate: send - TensorEdge((-31, max), (33, max)), inode_3_0 -> imce_3_3
+    var4 = 8224;
+    // generate. loop count == 1
+    __builtin_INODE_SEND(var4 + 0*32, 0, 10, 1);
+
+    // endgenerate
+    // endgenerate: send - TensorEdge((-31, max), (33, max)), inode_3_0 -> imce_3_3
+    // generate: sync all inodes
+    __builtin_INODE_SET_FLAG(255);
+    __builtin_INODE_STANDBY(0, 255);
+    __builtin_INODE_STANDBY(5, 255);
+    __builtin_INODE_STANDBY(10, 255);
+    __asm__ volatile("nop\n" "nop\n" "nop\n" "nop\n");
+    __builtin_INODE_SET_FLAG(0);
+    // endgenerate: sync all inodes
+    // generate: done and intrt for master inode
+    __builtin_INODE_DONE();
+    __builtin_INODE_INTRT(0);
+    // endgenerate: done and intrt for master inode
+    // generate: halt for master inode after done and intrt
+    __builtin_INODE_HALT();
+    // endgenerate: halt for master inode after done and intrt
+    // generate: recv: TensorID(35, func_out0)
+    var9 = (int*)(2048);
+    var11 = 0;
+    var10 = var9[0];
+    __asm__ volatile("nop");
+    for (int i1 = 0; i1 < var10; i1++) { // generate
+      __builtin_INODE_RECV(var11 + i1*32, 0, 0, 2);
+
+    } // endgenerate
+    // endgenerate: recv: TensorID(35, func_out0)
+    // generate: sync all inodes
+    __builtin_INODE_SET_FLAG(255);
+    __builtin_INODE_STANDBY(0, 255);
+    __builtin_INODE_STANDBY(5, 255);
+    __builtin_INODE_STANDBY(10, 255);
+    __asm__ volatile("nop\n" "nop\n" "nop\n" "nop\n");
+    __builtin_INODE_SET_FLAG(0);
+    // endgenerate: sync all inodes
+    // generate: done and intrt for master inode
+    __builtin_INODE_DONE();
+    __builtin_INODE_INTRT(0);
+    // endgenerate: done and intrt for master inode
+    // generate: halt for master inode after done and intrt
+    __builtin_INODE_HALT();
+    // endgenerate: halt for master inode after done and intrt
+  }
+}
