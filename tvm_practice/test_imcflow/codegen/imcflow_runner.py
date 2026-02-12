50d156c623c81d631b638c4b795d7036b2714a21
Jaeyong Jang <jong1664@gmail.com>
2026-02-12 22:38:14 +0900

    bugfix imcflow_runner.py

--- tvm_practice/test_imcflow/codegen/imcflow_runner.py
+++ tvm_practice/test_imcflow/codegen/imcflow_runner.py
@@ -192,7 +192,9 @@ class ImcFlowRunner(ABC):
 
         # Determine imc_size based on IMCFLOW_BIG_IMEM environment variable
         big_imem = os.getenv("IMCFLOW_BIG_IMEM", "").lower() in ("1", "true", "yes")
-        imc_size = "270464" if big_imem else "266368"
+        imc_size = 270464 if big_imem else 266368 # 0x42080 or 0x41080
+        imc_size += 32
+        imc_size = str(imc_size)
 
         # Get SRAM_BACKDOOR setting from environment (default: 1 for performance)
         sram_backdoor = os.getenv("SRAM_BACKDOOR", "1")
