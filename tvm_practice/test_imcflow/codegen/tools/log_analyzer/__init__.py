7c6105d55e2711aeeed7d752028db22567e46fed
Jaeyong Jang <jong1664@gmail.com>
2026-02-14 16:48:48 +0900

    update log analyzer

--- tvm_practice/test_imcflow/codegen/tools/log_analyzer/__init__.py
+++ tvm_practice/test_imcflow/codegen/tools/log_analyzer/__init__.py
@@ -5,7 +5,7 @@ Supports both RTL (fsim) and Python simulator (pysim) log formats.
 
 from .log_format import LogEntry, ParseError, parse_payload, parse_line, parse_file
 from .fast_search import fast_parse_file, fast_parse_files, grep_file
-from .models import FileStatus, PacketEvent, PacketTrace, SyncEvent, StallInfo
+from .models import FileStatus, PacketEvent, PacketTrace, SyncEvent, StallInfo, DurationRecord
 from .utils import parse_patterns, split_log_by_simulation
 
 __all__ = [
@@ -22,6 +22,7 @@ __all__ = [
     "PacketTrace",
     "SyncEvent",
     "StallInfo",
+    "DurationRecord",
     "parse_patterns",
     "split_log_by_simulation",
 ]
